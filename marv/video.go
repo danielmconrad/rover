4d9e4b0d3c6f3f15b42e0a5ab798e994d5364bf1
Daniel Conrad <daniel.m.conrad@gmail.com>
2019-04-22 21:13:40 -0500

    Restructures video listening

--- marv/video.go
+++ marv/video.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"io"
 	"log"
 	"net/http"
 	"os/exec"
@@ -14,93 +13,123 @@ import (
 )
 
 var (
-	ffmpegArgs = []string{
-		"-f", "v4l2", "-framerate", "25", "-video_size", "320x240", "-i", "/dev/video0",
-		"-f", "mpegts", "-codec:v", "mpeg1video", "-s", "320x240", "-b:v", "1000k", "-bf", "0", "pipe:1",
-	}
 	raspividArgs = []string{
 		"-t", "0", "-o", "-", "-w", "320", "-h", "240", "-fps", "12", "-pf", "baseline",
 	}
 	nalSeparator = []byte{0x00, 0x00, 0x00, 0x01}
+	width        = 320
+	height       = 240
 )
 
 func handleVideo(ctx context.Context) handlerFunc {
 	upgrader := websocket.Upgrader{}
 
-	clients := map[*websocket.Conn]bool{}
-
-	go func() {
-		for videoFragment := range startVideo() {
-			for client := range clients {
-				client.WriteMessage(websocket.BinaryMessage, videoFragment)
-			}
-		}
-	}()
-
 	return func(w http.ResponseWriter, req *http.Request) {
 		ws, err := upgrader.Upgrade(w, req, nil)
+		defer ws.Close()
 
 		if err != nil {
 			log.Println("upgrade error", err)
 			return
 		}
 
-		ws.WriteJSON(map[string]interface{}{
-			"action": "init",
-			"width":  320,
-			"height": 240,
+		wsCtx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		ws.SetCloseHandler(func(code int, text string) error {
+			cancel()
+			return nil
 		})
 
+		go handleWebsocket(wsCtx, ws)
+
 		for {
-			_, message, err := ws.ReadMessage()
-			if err != nil {
-				ws.Close()
+			select {
+			case <-ctx.Done():
 				return
-			}
-
-			if strings.HasPrefix(string(message), "REQUESTSTREAM") {
-				clients[ws] = true
-
-				ws.SetCloseHandler(func(code int, text string) error {
-					delete(clients, ws)
-					return nil
-				})
 			}
 		}
 	}
 }
 
-func startVideo() chan []byte {
-	dataChan := make(chan []byte)
+func handleWebsocket(ctx context.Context, ws *websocket.Conn) {
+	pauseChan := make(chan bool)
+	messageChan := make(chan []byte)
+
+	handleStartStream := func() {
+		for {
+			select {
+			case frame := <-startVideo(ctx):
+				ws.WriteMessage(websocket.BinaryMessage, frame)
+			case <-pauseChan:
+				return
+			case <-ctx.Done():
+				return
+			}
+		}
+	}
+
+	handleMessages := func() {
+		for {
+			_, message, err := ws.ReadMessage()
+			if err != nil {
+				return
+			}
+			messageChan <- message
+		}
+	}
+
+	handleMessage := func(message []byte) {
+		if strings.HasPrefix(string(message), "REQUESTSTREAM") {
+			go handleStartStream()
+		}
+
+		if strings.HasPrefix(string(message), "STOPSTREAM") {
+			pauseChan <- true
+		}
+	}
+
+	ws.WriteJSON(map[string]interface{}{
+		"action": "init",
+		"width":  width,
+		"height": height,
+	})
+
+	go handleMessages()
+
+	for {
+		select {
+		case message := <-messageChan:
+			handleMessage(message)
+		case <-ctx.Done():
+			break
+		}
+	}
+}
+
+func startVideo(ctx context.Context) chan []byte {
+	frameChan := make(chan []byte)
 
 	go func() {
-		// cmd := exec.Command("ffmpeg", ffmpegArgs...)
-		cmd := exec.Command("raspivid", raspividArgs...)
+		cmd := exec.CommandContext(ctx, "raspivid", raspividArgs...)
 
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
 			log.Println("stdout error", err)
 		}
 
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			log.Println("stderr error", err)
-		}
-
 		if err := cmd.Start(); err != nil {
 			log.Println("command start error", err)
 		}
 
-		scanner := bufio.NewScanner(io.MultiReader(stdout, stderr))
-
-		prevBytes := []byte{}
+		scanner := bufio.NewScanner(stdout)
+		scanner.Split(splitAtNALSeparator)
 
 		for scanner.Scan() {
-			allBytes := append(prevBytes, scanner.Bytes()...)
-
-			if idx := bytes.Index(allBytes[1:], nalSeparator); idx >= 0 {
-				dataChan <- allBytes[:idx+1]
-				prevBytes = allBytes[idx+1:]
+			select {
+			case frameChan <- scanner.Bytes():
+			case <-ctx.Done():
+				return
 			}
 		}
 
@@ -110,5 +139,21 @@ func startVideo() chan []byte {
 		}
 	}()
 
-	return dataChan
+	return frameChan
+}
+
+func splitAtNALSeparator(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+
+	if idx := bytes.Index(data[4:], nalSeparator); idx >= 0 {
+		return idx + 4, data[0 : idx+4], nil
+	}
+
+	if atEOF {
+		return len(data), data, nil
+	}
+
+	return 0, nil, nil
 }
--- static/video.js
+++ static/video.js
@@ -3,4 +3,4 @@ const wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
 
 wsavc.connect("ws://" + document.location.host + "/video");
 document.getElementById('video-container').appendChild(canvas);
-setTimeout(function() { wsavc.playStream() }, 2000);
+setTimeout(function() { wsavc.playStream() }, 3000);
